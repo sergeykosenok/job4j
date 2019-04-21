b8396ea0ce3686e3f21f62c246f4347fd19542e4
sergeykosenok <sergeykosenok@gmail.com>
2019-04-21 14:58:20 +0300

    1.3. JavaDoc. Code convention[#131560]

--- chapter_001/src/main/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
-package ru.job4j.calculate;
+﻿package ru.job4j.calculate;
 
+/**
+Calculate
+
+@author Sergey Kosenok (sergey.kosenok@gmail.com)
+
+Something smart will be write down later.
+*/
 public class Calculate{
+/**
+Main method
+This method is point where program starts
+@param args - arguments array
+*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, World!");
---
+++ chapter_001/src/main/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+﻿/**
+Package for calculating operations
+
+@author Sergey Kosenok
+@version 1.0
+@since 2019-04-21
+*/
+package ru.job4j.calculate;
