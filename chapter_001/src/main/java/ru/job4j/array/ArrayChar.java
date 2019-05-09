1af107d3d1287d2e16321e15c2b994745852f204
sergeykosenok <sergeykosenok@gmail.com>
2019-05-09 21:01:39 +0300

    6.4. Слово начинается с ...[#131537]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,4 +1,25 @@
 package ru.job4j.array;
 
 public class ArrayChar {
+
+    private char[] data;
+
+    public ArrayChar(String string) {
+        this.data = string.toCharArray();
+    }
+
+    public boolean startWith(String sample) {
+        char[] sampleCharArray = sample.toCharArray();
+
+        if (sampleCharArray.length > this.data.length) {
+            return false;
+        }
+
+        for (int index = 0; index < sampleCharArray.length; index++) {
+            if (this.data[index] != sampleCharArray[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -1,4 +1,28 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenPrefixLengthMoreLineLengthThenFalse() {
+        ArrayChar word = new ArrayChar("He");
+        boolean result = word.startWith("Hell");
+        assertThat(result, is(false));
+    }
 }
