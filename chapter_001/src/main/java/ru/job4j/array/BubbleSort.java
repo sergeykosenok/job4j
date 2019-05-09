4cc9473c88630ce1969b7b660e2ab55540181b40
sergeykosenok <sergeykosenok@gmail.com>
2019-05-09 23:25:54 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#131541]

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,4 +1,26 @@
 package ru.job4j.array;
 
 public class BubbleSort {
+
+    private double[] sortLeftToRight(double[] data, int lastChangesIndex) {
+        int lastChangesIndexNext = lastChangesIndex;
+        for (int index = 0; index < lastChangesIndex - 1; index++) {
+            double tempCell = data[index];
+            if (data[index] > data[index + 1]) {
+                //change each other cells
+                data[index] = data[index + 1];
+                data[index + 1] = tempCell;
+                lastChangesIndexNext = index + 1;
+            }
+        }
+        if (lastChangesIndex != lastChangesIndexNext) {
+            return this.sortLeftToRight(data, lastChangesIndexNext);
+        } else {
+            return data;
+        }
+    }
+
+    public double[] sort(double[] data) {
+        return this.sortLeftToRight(data, data.length);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -1,4 +1,26 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class BubbleSortTest {
+
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        BubbleSort arrayToSort = new BubbleSort();
+        double[] result = arrayToSort.sort(new double[]{1, 5, 4, 2, 3, 1, 7, 8, 0, 5});
+        double[] expected = new double[]{0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortArrayWithSevenElementsThenSortedArray() {
+        BubbleSort arrayToSort = new BubbleSort();
+        double[] result = arrayToSort.sort(new double[]{9, 0, 3, 7, 3, 0, 9});
+        double[] expected = new double[]{0, 0, 3, 3, 7, 9, 9};
+        assertThat(result, is(expected));
+    }
 }
+
