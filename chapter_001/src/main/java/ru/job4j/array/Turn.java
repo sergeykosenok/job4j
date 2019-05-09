261263e06566eba8298368fda9c00cc17e664dcd
sergeykosenok <sergeykosenok@gmail.com>
2019-05-09 20:29:40 +0300

    6.2. Перевернуть массив.[#131544]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,4 +1,15 @@
 package ru.job4j.array;
 
 public class Turn {
+
+    public double[] getBack(double[] data) {
+        int index = 0;
+        while (index < data.length - index - 1) {
+            double tempData = data[index];
+            data[index] = data[data.length - index - 1];
+            data[data.length - index - 1] = tempData;
+            index++;
+        }
+        return data;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -1,4 +1,34 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        double[] input = new double[] {4, 1, 6, 2};
+        double[] result = turner.getBack(input);
+        double[] expect = new double[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        double[] input = new double[] {4, 1, 6, 2, 7};
+        double[] result = turner.getBack(input);
+        double[] expect = new double[] {7, 2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOneAloneAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        double[] input = new double[] {7};
+        double[] result = turner.getBack(input);
+        double[] expect = new double[] {7};
+        assertThat(result, is(expect));
+    }
 }
