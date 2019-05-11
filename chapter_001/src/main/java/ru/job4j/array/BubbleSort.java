bf0b7cc0696e4f21095bab5d907596c3c6289d94
sergeykosenok <sergeykosenok@gmail.com>
2019-05-11 07:47:39 +0300

    BubbleSort class (method without recursion added)

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -20,7 +20,26 @@ public class BubbleSort {
         }
     }
 
+    private double[] sortLeftToRightWithoutRecursion(double[] data) {
+        int lastChangesIndex;
+        int lastChangesIndexNext = data.length;
+        do {
+            lastChangesIndex = lastChangesIndexNext;
+            for (int index = 0; index < lastChangesIndex - 1; index++) {
+                double tempCell = data[index];
+                if (data[index] > data[index + 1]) {
+                    //change each other cells
+                    data[index] = data[index + 1];
+                    data[index + 1] = tempCell;
+                    lastChangesIndexNext = index + 1;
+                }
+            }
+        } while (lastChangesIndex != lastChangesIndexNext);
+        return data;
+    }
+
     public double[] sort(double[] data) {
-        return this.sortLeftToRight(data, data.length);
+        //return this.sortLeftToRight(data, data.length);
+        return this.sortLeftToRightWithoutRecursion(data);
     }
 }
